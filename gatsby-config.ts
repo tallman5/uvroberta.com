32d11a2dc0dc9654f452675d50b593a979306e3b
tallman5 <tallman5@users.noreply.github.com>
2023-07-19 16:06:45 -0400

    updating config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,8 +5,6 @@ const config: GatsbyConfig = {
     title: `uvroberta.com`,
     siteUrl: `https://www.uvroberta.com`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
@@ -54,5 +52,7 @@ const config: GatsbyConfig = {
     },
   ]
 };
+
+console.log("AMK: " + process.env.AZ_MAP_KEY);
 
 export default config;
